d32cadd241254bce53632865b6d91ff37ab08ac0
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-03 20:56:28 -0300

    Update Lista01-Exercicio8.c

--- Lista01-Exercicio8.c
+++ Lista01-Exercicio8.c
@@ -1,3 +1,7 @@
+/*
+Faça um programa em C que receba os lados de um trapézio, calcule e mostre a área
+desse trapézio. Sabendo-se que: A = ((base maior + base menor) * altura )/2
+*/
 #include <stdio.h>
 int main (){
     int basemaior,basemenor,altura,A;
@@ -9,14 +13,6 @@ int main (){
     scanf("%d",&altura);
     A = ((basemaior + basemenor)*altura)/2;
     printf("A AREA DO TRAPEZIO EH-->%d",A);
-
-
-
-
-
-
-
-
 
  return 0;
 }

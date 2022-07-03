a2546c2dae74192c345d48c36fd9dada7b68ef23
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-03 20:43:24 -0300

    Update Lista01-Exercicio10.c

--- Lista01-Exercicio10.c
+++ Lista01-Exercicio10.c
@@ -1,3 +1,7 @@
+/*
+Faça um programa em C que receba os lados de um losango, calcule e mostre a área
+desse losango. Sabendo-se que: A = (diagonal maior * diagonal menor) /2
+*/
 #include <stdio.h>
 int main (){
   int diagonalMaior,diagonalMenor,A;
@@ -9,12 +13,6 @@ int main (){
 
     A = (diagonalMaior*diagonalMenor)/2;
     printf("A AREA DO LOSANGO EH-->  %d",A);
-
-
-
-
-
-
 
  return 0;
 }

c7ed126149f753920a2e7ede95af63a8e62f46ef
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-03 20:36:44 -0300

    Update Lista-01-Exercicio16.c

--- Lista-01-Exercicio16.c
+++ Lista-01-Exercicio16.c
@@ -1,3 +1,9 @@
+/*
+Sabe-se que para iluminar de maneira correta os cômodos de uma casa, para cada
+metro quadrado deve-se usar 18 W de potência. Faça um programa em C que receba as duas
+dimensões de um cômodo (em metros), calcule e mostre a sua área (em metros quadrados) e a potência
+de iluminação que deverá ser utilizada.
+*/
 #include <stdio.h>
 #define potencia 18
 int main (){
@@ -10,9 +16,5 @@ int main (){
   printf("A AREA DO COMODO EH %2.f\n",A);
    A *= potencia;
   printf("A PONTECIA DE ILUMINCAO DESTE DEVE SER %2.fW\n",A);
-
-
-
-
  return 0;
 }

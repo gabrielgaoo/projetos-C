e268ee3320fdafc659d6dabc85f9c2168ba0695e
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-03 20:28:52 -0300

    Update Lista-01-Exercicio13.c

--- Lista-01-Exercicio13.c
+++ Lista-01-Exercicio13.c
@@ -1,3 +1,11 @@
+/*
+Faça um programa em C que receba o ano de nascimento de uma pessoa e o ano
+atual, calcule e mostre:
+a) a idade dessa pessoa em anos;
+b) a idade dessa pessoa em meses;
+c) a idade dessa pessoa em dias;
+d) a idade dessa pessoa em semanas;
+*/
 #include <stdio.h>
 int main(){
   int AnoNascimento,AnoAtual,anos,meses,dias,semanas;

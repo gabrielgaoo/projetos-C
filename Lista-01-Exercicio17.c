51de2cfbd526e08a25f2bcb1baeed6ede4b1ae7d
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-03 20:37:42 -0300

    Update Lista-01-Exercicio17.c

--- Lista-01-Exercicio17.c
+++ Lista-01-Exercicio17.c
@@ -1,3 +1,8 @@
+/*
+Faça um programa em C que receba a medida de dois ângulos de um triângulo,
+calcule e mostre a medida do terceiro ângulo. Sabe-se que a soma dos ângulos de um
+triângulo é 180.
+*/
 #include <stdio.h>
 #define triangulo 180
 int main (){
@@ -9,15 +14,5 @@ int main (){
  scanf("%d",&anguloDois);
    A= triangulo-(anguloUm+anguloDois);
  printf("O TERCEIRO ANGULO EH %d",A);
-
-
-
-
-
-
-
-
-
-
  return 0;
 }

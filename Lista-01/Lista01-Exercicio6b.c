869b6dd3d9fb23318f74015f6efb393cec70862d
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-04 23:52:29 -0300

    Update Lista01-Exercicio6b.c

--- Lista-01/Lista01-Exercicio6b.c
+++ Lista-01/Lista01-Exercicio6b.c
@@ -1,21 +1,17 @@
+/*
+6) Faça um programa em C que receba o peso de uma pessoa, calcule e mostre:
+b) o novo peso se a pessoa emagrecer 20% sobre o peso digitado;
+*/
 #include <stdio.h>
 #define porcentagem 0.20
 int main(){
  float peso,emagreceu,resultado;
 
   printf("DIGITE SEU PESO--->");
-  scanf("%f",&peso);
-  emagreceu = peso*porcentagem;
-  resultado= peso-emagreceu;
+    scanf("%f",&peso);
+    emagreceu = peso*porcentagem;
+    resultado= peso-emagreceu;
   printf("VOCE EMAGRECEU %2.fKG",resultado);
-
-
-
-
-
-
-
-
 
 return 0;
 }

eba73ec707635a8668969a50192e352280af7985
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-05 00:13:44 -0300

    Update Lista-02-Exercicio3.c

--- Lista-02/Lista-02-Exercicio3.c
+++ Lista-02/Lista-02-Exercicio3.c
@@ -1,27 +1,21 @@
+/*
+Faça um programa em C que receba 2 números, e mostre o menor.
+*/
 #include <stdio.h>
  int main(){
-
  int numUM,numDois;
  printf ("DIGITE O PRIMEIRO NUMERO\n");
- scanf("%d",&numUM);
-
-printf ("DIGITE O SEGUNDO NUMERO\n");
- scanf("%d",&numDois);
- if(numUM>numDois){
+   scanf("%d",&numUM);
+ printf ("DIGITE O SEGUNDO NUMERO\n");
+   scanf("%d",&numDois);
+     if(numUM>numDois)
+     {
     printf("O PRIMEIRO NUMERO E MAIOR QUE O SEGUNDO\n");
- }
-  else if (numUM<numDois){
+      }
+        else if (numUM<numDois)
+           {
     printf("O SEGUNDO NUMERO E MAIOR QUE PRIMEIRO\n");
-  }
-
-
-
-
-
-
-
-
-
+           }
 
   return 0;
  }

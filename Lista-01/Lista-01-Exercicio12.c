f89cc528744c17f775c2bf7fa7742375a5293d0a
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-04 23:48:51 -0300

    Update Lista-01-Exercicio12.c

--- Lista-01/Lista-01-Exercicio12.c
+++ Lista-01/Lista-01-Exercicio12.c
@@ -1,19 +1,23 @@
+/*
+Faça um programa em C que calcule e mostre a tabuada de um número digitado
+pelo usuário.
+*/
 #include <stdio.h>
   int main (){
 
   int numero,resultado1,resultado2,resultado3,resultado4,resultado5,resultado6,resultado7,resultado8,resultado9,resultado10;
   printf("DIGITE UM NUMERO");
-  scanf("%d",&numero);
-  resultado1=numero*1;
-  resultado2=numero*2;
-  resultado3=numero*3;
-  resultado4=numero*4;
-  resultado5=numero*5;
-  resultado6=numero*6;
-  resultado7=numero*7;
-  resultado8=numero*8;
-  resultado9=numero*9;
-  resultado10=numero*10;
+     scanf("%d",&numero);
+    resultado1=numero*1;
+    resultado2=numero*2;
+    resultado3=numero*3;
+    resultado4=numero*4;
+    resultado5=numero*5;
+    resultado6=numero*6;
+    resultado7=numero*7;
+    resultado8=numero*8;
+    resultado9=numero*9;
+    resultado10=numero*10;
   printf("%dX1=%d\n",numero,resultado1);
   printf("%dX2=%d\n",numero,resultado2);
   printf("%dX3=%d\n",numero,resultado3);
@@ -24,15 +28,6 @@
   printf("%dX8=%d\n",numero,resultado8);
   printf("%dX9=%d\n",numero,resultado9);
   printf("%dX10=%d\n",numero,resultado10);
-
-
-
-
-
-
-
-
-
 
  return 0;
 }

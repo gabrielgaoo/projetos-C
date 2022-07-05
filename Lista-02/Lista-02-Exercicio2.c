5a650aac1dec42cb3ac8d15abb132eb843d82d1f
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-05 00:11:48 -0300

    Update Lista-02-Exercicio2.c

--- Lista-02/Lista-02-Exercicio2.c
+++ Lista-02/Lista-02-Exercicio2.c
@@ -1,21 +1,32 @@
+/*
+Faça um programa em C que receba duas notas, calcule e mostre a média aritmética e
+a mensagem que está na tabela a seguir:
+MÉDIA ARITMÉTICA MENSAGEM
+0,0 ●------○ 4,0 Reprovado
+4,0 ●------○ 7,0 Exame
+7,0 ●------● 10,0 Aprovado
+*/
 #include <stdio.h>
  int main (){
-
- int notaUm,notaDois,media;
- printf("DIGITE SUA PRIMEIRA NOTA\n");
- scanf("%d",&notaUm);
- printf("DIGITE SUA SEGUNDA NOTA\n");
- scanf("%d",&notaDois);
- media = (notaUm+notaDois)/2;
- printf("SUA MEDIA NA DISCIPLINA EH--->%d",media);
- if(media >=7){
+  int notaUm,notaDois,media;
+   printf("DIGITE SUA PRIMEIRA NOTA\n");
+     scanf("%d",&notaUm);
+   printf("DIGITE SUA SEGUNDA NOTA\n");
+    scanf("%d",&notaDois);
+    media = (notaUm+notaDois)/2;
+   printf("SUA MEDIA NA DISCIPLINA EH--->%d",media);
+  if(media >=7)
+  {
    printf("\nAPROVADO!!!");
- }
-   else if (media <=7&&media>=4){
-   printf("\nEXAME!!!");
-  }else if (media<=4){
-    printf("\nREPROVADO!!!");
   }
+     else if (media <=7&&media>=4)
+     {
+   printf("\nEXAME!!!");
+     }
+        else if (media<=4)
+           {
+    printf("\nREPROVADO!!!");
+           }
 
 
 

b2a52dffd6ba651f2b093edcb6fa3898fe822fe2
gabrielgaoo <106560166+gabrielgaoo@users.noreply.github.com>
2022-07-05 00:01:38 -0300

    Update Lista-01-Exercicio15.c

--- Lista-01/Lista-01-Exercicio15.c
+++ Lista-01/Lista-01-Exercicio15.c
@@ -1,22 +1,22 @@
-1#include <stdio.h>
+/*
+Faça um programa em C que receba o raio, calcule e mostre:
+a) o comprimento da esfera, sabe-se que C = 2πR;
+b) a área dessa esfera, sabe-se que A = πR²
+c) o volume dessa esfera, sabe-se que V = 4πR³/3
+*/
+#include <stdio.h>
 #define PI 3.14
 int main(){
   float raio,resultado,A,C,V;
   printf("DIGITE O RAIO DESTA ESFERA\n");
-  scanf("%f",&raio);
-  C = 2*PI*raio;
+     scanf("%f",&raio);
+      C = 2*PI*raio;
   printf("O CUMPRIMENTO DESTA ESFERA EH %2.f\n",C);
-  A = PI*raio*raio;
+      A = PI*raio*raio;
 
- printf("A AREA DESTA ESFERA EH %2.f\n",A);
- V =(4*PI*raio*raio*raio)/3;'	
- printf("O VOLUME DESTA ESFERA EH %2.f",V);
-
-
-
-
-
-
+  printf("A AREA DESTA ESFERA EH %2.f\n",A);
+      V =(4*PI*raio*raio*raio)/3;'	
+   printf("O VOLUME DESTA ESFERA EH %2.f",V);
 
  return 0;
 }
